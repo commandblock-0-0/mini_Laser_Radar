31e856f167be0b1d2373a5cde974c881aa0a3080
commandblock-0-0 <command_block163@163.com>
2023-03-07 20:12:10 +0800

    Basically complete the steering engine control function.

---
+++ LowerControl/esp32/ESP32S3_Radar/main/include/steering_control.h
@@ -0,0 +1,25 @@
+#ifndef _STRING_CONTROL_H_
+#define _STRING_CONTROL_H_
+
+#include "driver/ledc.h"
+
+#define LEDC_TIMER              LEDC_TIMER_0
+#define LEDC_MODE               LEDC_LOW_SPEED_MODE
+#define LEDC_DUTY_RES           LEDC_TIMER_13_BIT // Set duty resolution to 13 bits
+
+/*
+ *Use all parameters of steering gear
+*/
+typedef struct {
+    uint8_t angle_now;      //angle
+
+    uint32_t channel;       //pwm channel
+    ledc_mode_t speedmode;//pwm speed mode
+} xSteering_arguments_t;
+
+esp_err_t steering_init(ledc_timer_config_t *ledc_timer, 
+                        ledc_channel_config_t *config_arr,
+                        xSteering_arguments_t *Steering_arr);//arr no more than 5 item
+esp_err_t steering_ChangeAngle(xSteering_arguments_t *parguments, const uint8_t angle);
+
+#endif 
---
+++ LowerControl/esp32/ESP32S3_Radar/main/steering_control.c
@@ -0,0 +1,145 @@
+#include "driver/ledc.h"
+#include "steering_control.h"
+#include "sdkconfig.h"
+
+
+static uint32_t g_total_duty = 2;//Total value of duty cycle
+static float g_AngleBaseDutyNum;//Duty value corresponding to zero degree
+static float g_AngleMaxDutyNum;//Duty value corresponding to Max degree
+static float g_AngleStepLong;//Duty value corresponding to one degree
+
+esp_err_t steering_init(ledc_timer_config_t *ledc_timer, 
+                        ledc_channel_config_t *config_arr,
+                        xSteering_arguments_t *Steering_arr)//arr no more than 5 item
+{
+    int iconfigcount = CONFIG_STRING_NUM;
+
+    // Prepare and then apply the LEDC PWM timer configuration
+    ledc_timer->speed_mode       = LEDC_MODE;
+    ledc_timer->timer_num        = LEDC_TIMER_0;
+    ledc_timer->duty_resolution  = CONFIG_STRING_DUTY_RESOLUTION;
+    ledc_timer->freq_hz          = CONFIG_STRING_BASE_FREQUENCY;  // Set output frequency
+    ledc_timer->clk_cfg          = LEDC_AUTO_CLK;
+    ESP_ERROR_CHECK(ledc_timer_config(ledc_timer));
+
+    //Prepare and then apply the LEDC PWM channel configuration
+    // change the configuration file if you want to set
+    if (iconfigcount)//string 1
+    {
+        config_arr[0].speed_mode    = LEDC_MODE;
+        config_arr[0].channel       = LEDC_CHANNEL_0;
+        config_arr[0].timer_sel     = LEDC_TIMER;
+        config_arr[0].intr_type     = LEDC_INTR_DISABLE;
+        config_arr[0].gpio_num      = CONFIG_STRING_0_GPIO_NUM;//from sdkconfig
+        config_arr[0].duty          = 0; // Set duty to 0%
+        config_arr[0].hpoint        = 0;
+        ESP_ERROR_CHECK(ledc_channel_config(&config_arr[0]));
+        iconfigcount--;
+    }
+    if (iconfigcount)//string 2
+    {
+        #ifndef CONFIG_STRING_1_GPIO_NUM
+        #define CONFIG_STRING_1_GPIO_NUM 0
+        #endif
+        config_arr[1].speed_mode    = LEDC_MODE;
+        config_arr[1].channel       = LEDC_CHANNEL_1;
+        config_arr[1].timer_sel     = LEDC_TIMER;
+        config_arr[1].intr_type     = LEDC_INTR_DISABLE;
+        config_arr[1].gpio_num      = CONFIG_STRING_1_GPIO_NUM;//from sdkconfig
+        config_arr[1].duty          = 0; // Set duty to 0%
+        config_arr[1].hpoint        = 0;
+        ESP_ERROR_CHECK(ledc_channel_config(&config_arr[1]));
+        iconfigcount--;
+    }
+    if (iconfigcount)//string 3
+    {
+        #ifndef CONFIG_STRING_2_GPIO_NUM
+        #define CONFIG_STRING_2_GPIO_NUM 0
+        #endif
+        config_arr[2].speed_mode    = LEDC_MODE;
+        config_arr[2].channel       = LEDC_CHANNEL_2;
+        config_arr[2].timer_sel     = LEDC_TIMER;
+        config_arr[2].intr_type     = LEDC_INTR_DISABLE;
+        config_arr[2].gpio_num      = CONFIG_STRING_2_GPIO_NUM;//from sdkconfig
+        config_arr[2].duty          = 0; // Set duty to 0%
+        config_arr[2].hpoint        = 0;
+        ESP_ERROR_CHECK(ledc_channel_config(&config_arr[2]));
+        iconfigcount--;
+    }
+    if (iconfigcount)//string 4
+    {
+        #ifndef CONFIG_STRING_3_GPIO_NUM
+        #define CONFIG_STRING_3_GPIO_NUM 0
+        #endif
+        config_arr[3].speed_mode    = LEDC_MODE;
+        config_arr[3].channel       = LEDC_CHANNEL_3;
+        config_arr[3].timer_sel     = LEDC_TIMER;
+        config_arr[3].intr_type     = LEDC_INTR_DISABLE;
+        config_arr[3].gpio_num      = CONFIG_STRING_3_GPIO_NUM;//from sdkconfig
+        config_arr[3].duty          = 0; // Set duty to 0%
+        config_arr[3].hpoint        = 0;
+        ESP_ERROR_CHECK(ledc_channel_config(&config_arr[3]));
+        iconfigcount--;
+    }
+    if (iconfigcount)//string 5
+    {
+        #ifndef CONFIG_STRING_4_GPIO_NUM
+        #define CONFIG_STRING_4_GPIO_NUM 0
+        #endif
+        config_arr[4].speed_mode    = LEDC_MODE;
+        config_arr[4].channel       = LEDC_CHANNEL_4;
+        config_arr[4].timer_sel     = LEDC_TIMER;
+        config_arr[4].intr_type     = LEDC_INTR_DISABLE;
+        config_arr[4].gpio_num      = CONFIG_STRING_4_GPIO_NUM;//from sdkconfig
+        config_arr[4].duty          = 0; // Set duty to 0%
+        config_arr[4].hpoint        = 0;
+        ESP_ERROR_CHECK(ledc_channel_config(&config_arr[4]));
+    }
+
+    for (int i = 0; i < CONFIG_STRING_NUM; i++)
+    {
+        Steering_arr[i].angle_now  = CONFIG_STRING_DEFAULT_ANGLE;
+        Steering_arr[i].channel    = config_arr[i].channel;
+        Steering_arr[i].speedmode = config_arr[i].speed_mode;
+    }
+
+    for (int i = CONFIG_STRING_DUTY_RESOLUTION; i > 1; i--)
+        g_total_duty *= 2;//exponentiation
+
+    g_AngleBaseDutyNum = (float)CONFIG_STRING_MIN_HIGH_TIME * (float)CONFIG_STRING_BASE_FREQUENCY  
+                                * (float)g_total_duty / (float)1000000; //Duty value corresponding to zero degree
+    g_AngleMaxDutyNum = (float)CONFIG_STRING_MAX_HIGH_TIME * (float)CONFIG_STRING_BASE_FREQUENCY  
+                                * (float)g_total_duty / (float)1000000; //Duty value corresponding to Max degree
+    g_AngleStepLong = (g_AngleMaxDutyNum - g_AngleBaseDutyNum) / CONFIG_STRING_ANGLE_SCOPE; //Duty value corresponding to one degree
+
+    return ESP_OK;
+}
+
+/* Angle converted to duty value */
+static int AngleToDutyNum(const uint8_t angle)
+{
+    int DutyNum;
+
+    if(angle > CONFIG_STRING_ANGLE_SCOPE)
+        return -1;
+    
+    DutyNum = (int)(g_AngleBaseDutyNum + (float)angle * g_AngleStepLong);
+
+    return DutyNum;
+}
+
+esp_err_t steering_ChangeAngle(xSteering_arguments_t *parguments, const uint8_t angle)
+{
+    esp_err_t err;
+    parguments->angle_now = angle;
+
+    err = ledc_set_duty(parguments->speedmode, parguments->channel, 
+                            AngleToDutyNum(parguments->angle_now));
+    if (err)
+        return err;
+    err = ledc_update_duty(parguments->speedmode, parguments->channel);
+    if (err)
+        return err;
+    
+    return ESP_OK;
+}
