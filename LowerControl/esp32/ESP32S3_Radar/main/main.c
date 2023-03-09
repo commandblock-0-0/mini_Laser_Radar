566adc7c3c80317414fcd0579eb3c98d9736bed5
commandblock-0-0 <command_block163@163.com>
2023-03-09 19:39:34 +0800

    UART framework

--- LowerControl/esp32/ESP32S3_Radar/main/main.c
+++ LowerControl/esp32/ESP32S3_Radar/main/main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/queue.h"
 #include "esp_log.h"
 #include "driver/ledc.h"
+#include "driver/uart.h"
 #include "sdkconfig.h"
 #include "steering_control.h"
+#include "radar_UART.h"
 
 static ledc_timer_config_t g_Steering_Timer;
 static ledc_channel_config_t g_arrSteering_Channels[5];
@@ -25,8 +29,8 @@ void vSteering(void *data)
         if (i == CONFIG_STRING_ANGLE_SCOPE || i == 0)
             status = !status;
 
-        steering_ChangeAngle(&g_arrSteering_arguments[0], i);
-        steering_ChangeAngle(&g_arrSteering_arguments[1], i);
+        vSteering_ChangeAngle(&g_arrSteering_arguments[0], i);
+        vSteering_ChangeAngle(&g_arrSteering_arguments[1], i);
 
         vTaskDelay(10 / portTICK_PERIOD_MS);
     }
@@ -36,9 +40,18 @@ void app_main(void)
 {
     printf("here!\n");
 
-    steering_init(&g_Steering_Timer, g_arrSteering_Channels, g_arrSteering_arguments);
+    xRadar_UART_t* xRadar_uart_Opr;
+
+    vSteering_init(&g_Steering_Timer, g_arrSteering_Channels, g_arrSteering_arguments);
 
     xTaskCreate(vSteering, "SteeringTask", 2000, NULL, 5, NULL);
 
-    return;
+    //UART test
+    xRadar_uart_Opr = radar_UART_Run(NULL, NULL);
+    char* test_str = "This is a test string.\n";
+    while (1)   
+    {
+        uart_write_bytes(xRadar_uart_Opr->uart_num, (const char*)test_str, strlen(test_str));
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
 }
